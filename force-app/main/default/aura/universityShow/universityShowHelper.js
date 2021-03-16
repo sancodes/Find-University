331726871076a5e0d5df6b725cd03565bb348771
sancodes <munsan14@gmail.com>
2021-03-16 14:17:01 -0700

    remove the functions and put into respective components helper

--- force-app/main/default/aura/universityShow/universityShowHelper.js
+++ force-app/main/default/aura/universityShow/universityShowHelper.js
@@ -1,42 +1,5 @@
 ({
-	searchUniFnhelperMethod : function(component, event) {
-        
-		let country = component.get("v.inputFieldSearchCountry");
-        let action = component.get("c.getListOfUniversitiesInACountry");
-        action.setParams({"place":country});
-        action.setCallback(this, function(res){
-			console.log(res.getState());
-            console.log(res.getReturnValue()[0]);
-            let state = res.getState();
-            if(state === "SUCCESS"){
-                component.set("v.resultValue", res.getReturnValue());
-                
-            }
-        })
-        $A.enqueueAction(action);
-	},
-    doSaveHelperMethod: function(component, event){
-        //let universityObject = component.get("v.uniObj");
-        let universityObject = event.getSource().get('v.value');
-        //console.log('hi' + event.getSource().get('v.value').Name);
-    	let action = component.get('c.saveToUniversityObject');
-        action.setParams({'uniObj': universityObject});
-        action.setCallback(this, function(res){
-            let state = res.getState();
-            if(state === "SUCCESS"){
-                component.set("v.universityId", res.getReturnValue());
-            }
-        })
-        $A.enqueueAction(action);
-	},
-    showUniversitiesHelperMethod: function(component, event){
-    	let action = component.get('c.retrieveUniversities');
-        action.setCallback(this, function(res){
-            let state = res.getState();
-            if(state === "SUCCESS"){
-                component.set("v.universityList", res.getReturnValue());
-            }
-        })
-        $A.enqueueAction(action);
-	},
+    helperMethod : function() {
+		
+	}
 })
